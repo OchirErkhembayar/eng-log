f7514eacc0b3b2575404a9d672c8c31e57bd5055
OchirErkhembayar <ochir_erkhembayar@yahoo.com>
2023-11-21 09:31:21 +0000

    Flip footer

--- src/ui.rs
+++ src/ui.rs
@@ -12,12 +12,16 @@ pub fn ui(f: &mut Frame, app: &mut App) {
     let chunks = Layout::default()
         .direction(Direction::Vertical)
         .constraints([
+            // Title
             Constraint::Length(3),
+            // Body
             Constraint::Min(1),
+            // Footer
             Constraint::Length(3),
         ])
         .split(f.size());
 
+    // Title
     {
         let title_chunks = Layout::default()
             .direction(Direction::Horizontal)
@@ -46,6 +50,7 @@ pub fn ui(f: &mut Frame, app: &mut App) {
         f.render_widget(date, title_chunks[1]);
     }
 
+    // Body
     {
         let mut list_items = Vec::<ListItem>::new();
 
@@ -61,32 +66,32 @@ pub fn ui(f: &mut Frame, app: &mut App) {
         f.render_widget(list, chunks[1]);
     }
 
+    // Footer
     {
         let footer_chunks = Layout::default()
             .direction(Direction::Vertical)
             .constraints([Constraint::Length(1), Constraint::Length(1)])
             .split(chunks[2]);
+
         if app.current_screen == CurrentScreen::Editing {
             let input_buffer_text = Span::styled(
                 format!("> {}{}", app.note_buffer, "█"),
                 Style::default().fg(Color::Yellow),
             );
             let input_buffer = Paragraph::new(Line::from(input_buffer_text));
-            f.render_widget(input_buffer, footer_chunks[1]);
+            f.render_widget(input_buffer, footer_chunks[0]);
         }
 
-        {
-            let current_keys_hint = {
-                let text = match app.current_screen {
-                    CurrentScreen::Main => "(q) quit | (i) edit",
-                    CurrentScreen::Editing => "(esc) back | (enter) save",
-                };
-                Span::styled(text, Style::default().fg(Color::Yellow))
+        let current_keys_hint = {
+            let text = match app.current_screen {
+                CurrentScreen::Main => "(q) quit | (i) edit",
+                CurrentScreen::Editing => "(esc) back | (enter) save",
             };
-            let key_hints_footer = Paragraph::new(Line::from(current_keys_hint))
-                .block(Block::default().padding(Padding::horizontal(1)));
+            Span::styled(text, Style::default().fg(Color::Yellow))
+        };
+        let key_hints_footer = Paragraph::new(Line::from(current_keys_hint))
+            .block(Block::default().padding(Padding::horizontal(1)));
 
-            f.render_widget(key_hints_footer, footer_chunks[0]);
-        }
+        f.render_widget(key_hints_footer, footer_chunks[1]);
     }
 }
